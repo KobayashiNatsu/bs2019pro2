1d4713aa7f3b9bb65b469c2591a17cf03d07db9a
KobayashiNatsu <summer0609takos@icloud.com>
2019-06-20 19:59:17 +0900

    モデル追加とかしたよ

---
+++ Assets/Script/Model_01/Beam_Color.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam_Color : MonoBehaviour {
+
+
+    ParticleSystem p;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        p = GetComponent<ParticleSystem>();
+        var main = p.main;
+        if (Light_RorB.Get_color()){
+            main.startColor = Color.red;
+        }
+        else {
+            main.startColor = Color.blue;
+        }
+
+    }
+}
